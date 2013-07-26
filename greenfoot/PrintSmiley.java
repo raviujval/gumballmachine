4a393daf5d62ccffa488d3044ecbda1aa6285fed
murthybsn <murthybsn@gmail.com>
2013-07-25 20:00:07 -0700

    adding smiley and honeydip options

---
+++ greenfoot/PrintSmiley.java
@@ -0,0 +1,53 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class printSmiley here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PrintSmiley extends Actor
+{
+    /**
+     * Act - do whatever the printSmiley wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public PrintSmiley () 
+    {
+        GreenfootImage image = getImage() ;
+        image.scale( 50 , 50 ) ; 
+    }
+    public void act() 
+    {
+       int mouseX, mouseY ;
+
+        if(Greenfoot.mousePressed(this)) {          
+            MouseInfo mouse = Greenfoot.getMouseInfo();  
+            mouseX=mouse.getX();  
+            mouseY=mouse.getY(); 
+            GumballMachine.togglesmileystatus() ;
+            
+            if(! GumballMachine.printsmiley) {
+                
+            Message m = new Message() ;
+            m.setText( "Regular is also good!" ) ;
+            World world = getWorld() ;
+            world.addObject(m, 270,420) ;
+
+            
+        }
+        
+            else if (GumballMachine.printsmiley) {
+                
+            Message m = new Message() ;
+            m.setText( "Smiley Gumball !!" ) ;
+            World world = getWorld() ;
+            world.addObject(m, 270,420) ;
+ 
+        }
+        
+    }    
+}
+
+}
