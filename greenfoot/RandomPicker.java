dff0db39d262361827d4564363f2d5c7e7f88e12
taksaito <tsaito@hortonworks.com>
2013-07-27 14:20:22 -0700

    Changed RandomPicker

--- greenfoot/RandomPicker.java
+++ greenfoot/RandomPicker.java
@@ -12,26 +12,34 @@ public class RandomPicker extends Picker
     public void getGumball() {
        World world = getWorld() ;
        GumballMachine gumballmachine = GumballMachine.getInstance();
-       
        Boolean isAnyGumballLeft = false ;
-       //Message is displayed
-       if (isAnyGumballLeft) {
-           Message m = new Message() ;
+       Message m = new Message() ;
+       
+       if (gumballmachine.getGreenGumballCount()>0) {
            m.setText( "I'll pick green!" ) ;
-           world.addObject(m, 500, 291) ;
            GreenGumball gg = new  GreenGumball() ;
-           world.addObject(gg, 369, 389) ;
+           this.dispenseGumball(m,gg) ;
+        } else if (gumballmachine.getRedGumballCount()>0) {
+           m.setText( "I'll pick red!" ) ;
+           RedGumball rg = new  RedGumball() ;
+           this.dispenseGumball(m,rg) ;
+        } else if (gumballmachine.getBlueGumballCount()>0) {
+           m.setText( "I'll pick blue!" ) ;
+           BlueGumball rg = new  BlueGumball() ;
+           this.dispenseGumball(m,rg) ;
         } else {
-           Message m = new Message() ;
-           m.setText( "No green left!" ) ;
+           m.setText( "No more Gumball left!" ) ;
            world.addObject(m, 500, 291) ; 
-           if (successor != null) {
-               successor.getGumball() ;
-            }
         }
     }
     
     public void setSuccessor (Picker picker) {
         successor = picker;
+    }
+    
+    public void dispenseGumball(Message m, Gumball gumball) {
+         World world = getWorld() ;
+         world.addObject(m, 500, 291) ;
+         world.addObject(gumball, 369, 389) ;
     }
 }
